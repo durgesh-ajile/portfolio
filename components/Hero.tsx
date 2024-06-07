44ee2e22083ac64555f3a84fabe3cfb3b8987100
Durgesh rajak <durgeshrajak254@gmail.com>
2024-06-07 13:22:45 +0530

    changed hero name position

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,12 +52,12 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Hi! I&apos;m Durgesh, a Full Stack Developer based in India."
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Durgesh, a Full Stack Developer based in India.
+            Transforming Concepts into Seamless User Experiences
           </p>
 
           <a href="#about">
